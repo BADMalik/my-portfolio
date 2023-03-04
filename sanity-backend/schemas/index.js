5ee80f5fbf5fdac07f5359c11727e6db79fde087
BADMalik <malikzaman133@gmail.com>
2023-03-04 13:59:30 +0500

    completed schema

--- sanity-backend/schemas/index.js
+++ sanity-backend/schemas/index.js
@@ -1,4 +1,21 @@
-import socials from './socials'
 import testamonials from './testamonials'
+import abouts from './abouts'
+import socials from './socials'
+import skills from './skills'
+import workExperience from './workExperience'
+import works from './works'
+import experiences from './experiences'
+import contact from './contact'
+import brands from './brands'
 
-export const schemaTypes = [testamonials, socials]
+export const schemaTypes = [
+  testamonials,
+  socials,
+  abouts,
+  skills,
+  workExperience,
+  experiences,
+  contact,
+  works,
+  brands,
+]
